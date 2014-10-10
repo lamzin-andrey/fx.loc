6908fa9d7654a61855fdb4cc08bc5851b188f31e
andrey <andrey@andrey-NULL>
2014-10-10 14:10:09 +0400

    Добавлена страничка /keywords

--- www/programming_fundamentals/tpl/qs_tasklist.tpl.php
+++ www/programming_fundamentals/tpl/qs_tasklist.tpl.php
@@ -2,5 +2,6 @@
 	<li><a class="" href="<?=WEB_ROOT?>/quick_start/"><?=$lang['intro']?></a></li>
 	<li><a class="" href="<?=WEB_ROOT?>/quick_start/wtf/"><?=$lang['wtf']?></a></li>
 	<li><a class="" href="<?=WEB_ROOT?>/quick_start/security/"><?=$lang['security']?></a></li>
+	<li><a class="" href="<?=WEB_ROOT?>/quick_start/keywords/"><?=$lang['keywords']?></a></li>
 </ul>
 
