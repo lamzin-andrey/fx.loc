e2cf28d90d7e96abf5459459df94c2a91c56a390
andrey <andrey@andrey-NULL>
2014-10-10 12:09:38 +0400

    Добавлена страничка про безопасность

--- www/programming_fundamentals/tpl/qs_tasklist.tpl.php
+++ www/programming_fundamentals/tpl/qs_tasklist.tpl.php
@@ -1,5 +1,6 @@
 <ul class="scroll-y">
 	<li><a class="" href="<?=WEB_ROOT?>/quick_start/"><?=$lang['intro']?></a></li>
 	<li><a class="" href="<?=WEB_ROOT?>/quick_start/wtf/"><?=$lang['wtf']?></a></li>
+	<li><a class="" href="<?=WEB_ROOT?>/quick_start/security/"><?=$lang['security']?></a></li>
 </ul>
 
